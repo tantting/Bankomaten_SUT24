da951790ad082fcc647f0287bc9b296811ae07f2
tantting <jennyann.hayward82@gmail.com>
2024-09-23 09:58:55 +0200

    Add start of program, user choosing to log-in or shut down.

--- CashMachine_Lab4/Program.cs
+++ CashMachine_Lab4/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Välkommen till bankomaten");
+        bool runApp = true;
+        while (runApp)
+        {
+            Console.WriteLine("Välkommen till din internetbank" +
+                              "[1] Logga in" +
+                              "[2] Stäng ner");
+
+            switch (Console.ReadLine())
+            {
+                case 1:
+                    //köra programmet 
+                    break;
+                case 2:
+                    runApp = false;
+                    break;
+                default:
+                    Console.WriteLine("Felaktigt val! Försök igen!");
+                    break;
+            }
+
+        }
     }
 }
